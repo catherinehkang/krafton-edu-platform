51b5cd98abedca9dc5099c96af71b2a12f339c78
catherinehkang <hyunjin@bluehole.net>
2026-06-17 12:40:36 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDAFsqk3D01syYSWJXFDClK6x_agpnGytw",
+  authDomain: "krafton-ways-of-working.firebaseapp.com",
+  projectId: "krafton-ways-of-working",
+  storageBucket: "krafton-ways-of-working.firebasestorage.app",
+  messagingSenderId: "548671868473",
+  appId: "1:548671868473:web:eead8f74cc2030efbac7e8",
+  measurementId: "G-R3GXJ9SPFX"
+};
